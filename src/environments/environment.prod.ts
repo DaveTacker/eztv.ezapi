39f29e526ac8853fa44629378b3e46ea84a12b1c
Dave Tacker <david.tacker@gmail.com>
2019-02-24 09:09:21 -0800

    added prod firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDmDS5nrnaLYf2qNAMXbTQCUSJAZBd8BZo',
+    authDomain: 'ezapi-ed8d6.firebaseapp.com',
+    databaseURL: 'https://ezapi-ed8d6.firebaseio.com',
+    projectId: 'ezapi-ed8d6',
+    storageBucket: 'ezapi-ed8d6.appspot.com',
+    messagingSenderId: '157304973371'
+  }
 };
